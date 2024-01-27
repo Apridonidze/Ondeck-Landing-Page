82c95e4d2eee235316155723acf9b191a47fe934
Apridonidze <129777060+Apridonidze@users.noreply.github.com>
2024-01-27 18:27:19 +0400

    Add files via upload

---
+++ Ondeck Landing Page/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".toggleDiv").onclick = function () {
+    document.querySelector(".toggleDiv").style.display = "none";
+}
+
+
+
+
+document.querySelector("#menuImg").onclick = function(){
+    document.querySelector(".toggleDiv").style.display = "block";
+}
